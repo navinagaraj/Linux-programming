4832dec05072314480dcc4e45ebc904f43d8d90b
Navinkumar <navinagaraj18@gmail.com>
2019-01-26 07:47:35 +0530

    shell-command for " ls "

---
+++ shell-command/header.h
@@ -0,0 +1,27 @@
+/*****************************************/
+/* Author       : Navin kumar.N          */
+/* Date         : 25-01-2019             */
+/* Filename     : READMI.txt             */
+/* Description  : header.h  In " LS "    */
+/*****************************************/
+
+#ifndef HEADER_H
+#define HEADER_H
+
+/* Included header files */
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <sys/types.h>
+#include  <dirent.h>
+#include  <sys/stat.h>
+#include  <fcntl.h>
+#include  <error.h>
+#include  <string.h>
+/* Macro Definitions */
+
+/* user defined datatype definitions */
+
+/* function prototypes */
+
+
+#endif //HEADER_H
---
+++ shell-command/ls.c
@@ -0,0 +1,44 @@
+#include  "header.h"
+
+struct dirent *data;
+
+int main(int argc,char *dat[])
+/*command line argument */
+{
+  int a=0,b=0;
+  DIR *n,*p;
+  char *ptr,ptr1;
+  ptr=dat[1];
+
+  if(argc==1)
+  {
+    n=opendir("./");
+    /*The   opendir()  function  opens  a  directory stream corresponding to  the  directory*/ 
+  }
+  else if(argc >1)
+  {
+   n=opendir(ptr);
+   if(n==0)
+   {
+     printf("Not directory");
+     exit(0);
+     /* exit from the program*/
+   }
+  }
+  data = readdir(n);
+  /* The fnction returns a pointer to a strcture next ditectory stream point */ 
+  for(;data !=NULL;) 
+  {
+    if(strcmp(data->d_name,".")==0||strcmp(data->d_name,"..")==0)
+      {
+        /*To avoiding the "." and ".." from the data.*/
+      }
+    else
+      printf("%s\n",data->d_name);
+      /*printing data in the struct*/
+  data=readdir(n);
+  /*The fnction returns a pointer to a strcture next ditectory stream point*/
+  }
+  closedir(n);
+  /*The  closedir  function closes the directory stream*/
+}
