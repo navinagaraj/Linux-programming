4bfcd38bfea692d698e5d419a398ab2412cd8264
Navinkumar <navinagaraj18@gmail.com>
2019-01-26 10:10:38 +0530

    Shell-command "ls"

---
+++ shell-commands/ls/README.txt
@@ -0,0 +1,14 @@
+/*****************************************/
+/* Author       : Navin kumar.N          */
+/* Date         : 25-01-2019             */
+/* Filename     : READMI.txt             */
+/* Description  : Explanation of " LS "  */
+/*****************************************/
+
+STEP 1: Name your executable as my_ls
+
+STEP 2: Executing my_ls without parameters has to take the listing directory as current directory.
+
+STEP 3: Executing my_ls with one argument has to check wether the passed argument is a directory.
+
+STEP 4: Executing my_ls with one argument has to read the directory contents and display the list of files and directory mentioned in the directory mentioned in the argument.
--- shell-commands/ls/ls.c
+++ shell-commands/ls/ls.c
@@ -5,22 +5,22 @@
 /* Description  : ls.c file  In " LS "   */
 /*****************************************/
 
-#include  "header.h"
 
+#include  "header.h"
 struct dirent *data;
 
 int main(int argc,char *dat[])
-/*command line argument */
+/*command line interface*/
 {
   int a=0,b=0;
   DIR *n,*p;
   char *ptr,ptr1;
   ptr=dat[1];
-
   if(argc==1)
   {
     n=opendir("./");
-    /*The   opendir()  function  opens  a  directory stream corresponding to  the  directory*/ 
+    /* The   opendir function  opens  a  directory
+       stream corresponding to  the  directory */
   }
   else if(argc >1)
   {
@@ -29,23 +29,22 @@ int main(int argc,char *dat[])
    {
      printf("Not directory");
      exit(0);
-     /* exit from the program*/
+     /*Exit from the program*/
    }
   }
   data = readdir(n);
-  /* The fnction returns a pointer to a strcture next ditectory stream point */ 
-  for(;data !=NULL;) 
+  /*read a directory function returns a pointer */
+  for(;data !=NULL;)
   {
     if(strcmp(data->d_name,".")==0||strcmp(data->d_name,"..")==0)
       {
-        /*To avoiding the "." and ".." from the data.*/
+        /*To avoiding the "." and ".."*/
       }
     else
       printf("%s\n",data->d_name);
-      /*printing data in the struct*/
   data=readdir(n);
-  /*The fnction returns a pointer to a strcture next ditectory stream point*/
+  /*read a directory function returns a pointer */
   }
   closedir(n);
-  /*The  closedir  function closes the directory stream*/
+  /*close a directory*/
 }
