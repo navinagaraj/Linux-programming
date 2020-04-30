d46a8166d826705b929a12bc78d7d46616cdb936
navinagaraj <navinagaraj18@gmail.com>
2020-05-01 00:00:34 +0530

    DUP

---
+++ dup/dup_creat/dup.c
@@ -0,0 +1,39 @@
+#include "dup.h"
+
+int main(){
+	char buffer[121],buffer_1[121],w_buffer[62]="This is a normal fd data This is not a normal fd is a dup 1 ",w_buffer_2[61]="This 2 is a normal fd data This is not a normal fd is a dup\n";
+	int open_fd,dup_fd,a,b,retur,data;
+	ssize_t read_fd,write_fd,close_fd,fd;
+
+	open_fd = open("./sample.txt", O_RDWR|O_APPEND);i//open and possibly create a file//
+	perror("open");
+	dup_fd  = dup(open_fd);// duplicate a file descriptor//
+	perror("dup");
+
+	fd = fork();// create a child process//
+	if(fd > 0){
+		printf("I am Parent\n");
+		 }
+	else if(fd == 0){
+	write_fd = write(open_fd, w_buffer, strlen(w_buffer));
+	printf("%ld\n",write_fd);
+	write_fd = write(dup_fd, w_buffer_2, strlen(w_buffer_2));
+	printf("%d\n",b = write_fd);
+	return 10;}
+	else{
+		printf("Fork not created\n");}
+
+
+	read_fd = read(open_fd, buffer, 5);
+	//perror("read");
+	printf("%ld\n",read_fd);
+	printf("buffer   = %s\n",buffer);
+
+	read_fd =read(dup_fd, buffer_1,5);
+       //perror("read");
+	printf("%ld\n",read_fd);
+	printf("buffer_1 = %s\n",buffer_1);
+
+	close_fd = close(open_fd);
+	close_fd = close(dup_fd);
+}
---
+++ dup/dup_creat/dup.h
@@ -0,0 +1,22 @@
+/*******************************************/
+/* Author       : Navin kumar.N            */
+/* Date         : 30-04-2020               */
+/* Filename     : dup.H                    */
+/* Description  : HEADER FILE  "dup"       */
+/*******************************************/
+
+#ifndef dup_h
+#define dup_h
+
+#include <stdio.h>
+#include <unistd.h>
+#define _GNU_SOURCE             /* See feature_test_macros(7) */
+#include <fcntl.h>              /* Obtain O_* constant definitions */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <error.h>
+#include <string.h>
+
+#endif  //dup_h
