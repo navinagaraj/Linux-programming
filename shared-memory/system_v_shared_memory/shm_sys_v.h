e76d1f499d9f5ac9fc4223caa8f6c62181531b4d
navinagaraj <navinagaraj18@gmail.com>
2020-05-19 14:29:37 +0530

    system_v_shared_memory

---
+++ shared-memory/system_v_shared_memory/shm_sys_v.h
@@ -0,0 +1,29 @@
+/**********************************************/
+/* Author       : Naveenkumar N               */
+/* Date         : 19-05-2020                  */
+/* Filename     : shm_sys_v.h                 */
+/* Description  : shared-memory header files  */
+/**********************************************/
+
+
+#ifndef SHM_SYS_V_H
+#define SHM_SYS_V_H
+
+/* Included header files */
+
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <error.h>
+#include <string.h>
+
+
+/* Macro Definitions */
+
+/* Functions prototype */
+
+
+/* user defined datatype definitions */
+
+#endif //SHM_SYS_V_H
